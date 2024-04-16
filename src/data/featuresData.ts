f39bc1add68e0adc9d087ae913c0fa7c085d4e45
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-16 18:56:22 +0200

    features data created

---
+++ src/data/featuresData.ts
@@ -0,0 +1,92 @@
+const features = [
+  {
+    title: "Inventory Management",
+    icon: "ri-equalizer-3-line",
+    shortDesc: "Get real-time inventory information",
+    subItems: [
+      {
+        title: "Title",
+        icon: "ri-stack-line",
+        longDesc: "Long Description",
+      },
+      {
+        title: "Title",
+        icon: "ri-stack-line",
+        longDesc: "Long Description",
+      },
+      {
+        title: "Title",
+        icon: "ri-stack-line",
+        longDesc: "Long Description",
+      },
+    ],
+  },
+  {
+    title: "Inventory Reporting",
+    icon: "ri-file-chart-line",
+    shortDesc: "Create detailed reports based on your needs",
+    subItems: [
+      {
+        title: "Title",
+        icon: "ri-stack-line",
+        longDesc: "Long Description",
+      },
+      {
+        title: "Title",
+        icon: "ri-stack-line",
+        longDesc: "Long Description",
+      },
+      {
+        title: "Title",
+        icon: "ri-stack-line",
+        longDesc: "Long Description",
+      },
+    ],
+  },
+  {
+    title: "Advanced Filtering",
+    icon: "ri-filter-3-line",
+    shortDesc: "Create your own filters for better management",
+    subItems: [
+      {
+        title: "Title",
+        icon: "ri-stack-line",
+        longDesc: "Long Description",
+      },
+      {
+        title: "Title",
+        icon: "ri-stack-line",
+        longDesc: "Long Description",
+      },
+      {
+        title: "Title",
+        icon: "ri-stack-line",
+        longDesc: "Long Description",
+      },
+    ],
+  },
+  {
+    title: "High Customizability",
+    icon: "ri-tools-line",
+    shortDesc: "Customize your inventory to your own needs",
+    subItems: [
+      {
+        title: "Title",
+        icon: "ri-stack-line",
+        longDesc: "Long Description",
+      },
+      {
+        title: "Title",
+        icon: "ri-stack-line",
+        longDesc: "Long Description",
+      },
+      {
+        title: "Title",
+        icon: "ri-stack-line",
+        longDesc: "Long Description",
+      },
+    ],
+  },
+];
+
+export default features;
