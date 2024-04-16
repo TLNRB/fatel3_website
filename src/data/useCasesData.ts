2d562bb414a0d60e473ebbba3ae584757a0a82a2
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-16 19:04:30 +0200

    use cases data created

--- src/data/useCasesData.ts
+++ src/data/useCasesData.ts
@@ -1,92 +1,18 @@
-const features = [
+const useCases = [
   {
-    title: "Inventory Management",
-    icon: "ri-equalizer-3-line",
-    shortDesc: "Get real-time inventory information",
-    subItems: [
-      {
-        title: "Title",
-        icon: "ri-stack-line",
-        longDesc: "Long Description",
-      },
-      {
-        title: "Title",
-        icon: "ri-stack-line",
-        longDesc: "Long Description",
-      },
-      {
-        title: "Title",
-        icon: "ri-stack-line",
-        longDesc: "Long Description",
-      },
-    ],
+    title: "Reselling",
+    route: "/reselling",
+    icon: "ri-price-tag-3-line",
+    shortDesc: "For the ones reselling various items and products",
+    commingSoon: false,
   },
   {
-    title: "Inventory Reporting",
-    icon: "ri-file-chart-line",
-    shortDesc: "Create detailed reports based on your needs",
-    subItems: [
-      {
-        title: "Title",
-        icon: "ri-stack-line",
-        longDesc: "Long Description",
-      },
-      {
-        title: "Title",
-        icon: "ri-stack-line",
-        longDesc: "Long Description",
-      },
-      {
-        title: "Title",
-        icon: "ri-stack-line",
-        longDesc: "Long Description",
-      },
-    ],
-  },
-  {
-    title: "Advanced Filtering",
-    icon: "ri-filter-3-line",
-    shortDesc: "Create your own filters for better management",
-    subItems: [
-      {
-        title: "Title",
-        icon: "ri-stack-line",
-        longDesc: "Long Description",
-      },
-      {
-        title: "Title",
-        icon: "ri-stack-line",
-        longDesc: "Long Description",
-      },
-      {
-        title: "Title",
-        icon: "ri-stack-line",
-        longDesc: "Long Description",
-      },
-    ],
-  },
-  {
-    title: "High Customizability",
-    icon: "ri-tools-line",
-    shortDesc: "Customize your inventory to your own needs",
-    subItems: [
-      {
-        title: "Title",
-        icon: "ri-stack-line",
-        longDesc: "Long Description",
-      },
-      {
-        title: "Title",
-        icon: "ri-stack-line",
-        longDesc: "Long Description",
-      },
-      {
-        title: "Title",
-        icon: "ri-stack-line",
-        longDesc: "Long Description",
-      },
-    ],
+    title: "E-commerce",
+    route: "/e-commerce",
+    icon: "ri-shopping-cart-line",
+    shortDesc: "Integration with the most used e-commerce platforms",
+    commingSoon: true,
   },
 ];
 
-export default features;
+export default useCases;
