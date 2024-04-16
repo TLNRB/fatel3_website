db2e2d56671b1fe4ac745aa7f497a247096e9c0e
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-16 18:16:51 +0200

    Navbar created

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+/*-- Import Components --*/
+import Navbar from '@/components/Misc/Navbar.vue'
 </script>
 
 <template>
+  <Navbar />
   <RouterView />
 </template>
 
