a987f376aa3209830bf0225dfb084b973a55816e
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-19 11:09:36 +0200

    navbar desktop layout progeress

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,11 @@ import Navbar from "@/components/Nav/Navbar.vue";
 <template>
   <Navbar />
   <RouterView />
+  <div
+    class="hidden bg-red-500 h-[150vh] w-[100&] flex justify-center items-center"
+  >
+    hello
+  </div>
 </template>
 
 <style scoped></style>
