c57dc52ec4184467fefb6b88b4853b4541769fac
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-20 12:42:34 +0200

    nav items route update

--- src/data/resourcesData.ts
+++ src/data/resourcesData.ts
@@ -4,12 +4,14 @@ const resources = [
     subItems: [
       {
         title: "About us",
+        route: "/about",
         icon: "ri-information-2-line",
         shortDesc: "Our story, team, mission and goals",
         commingSoon: false,
       },
       {
         title: "Blog",
+        route: "/blog",
         icon: "ri-article-line",
         shortDesc: "Check out the latest new and updates",
         commingSoon: true,
@@ -21,18 +23,21 @@ const resources = [
     subItems: [
       {
         title: "Support",
+        route: "/support",
         icon: "ri-customer-service-2-line",
         shortDesc: "Get in touch with fatel3 to get the help you need",
         commingSoon: false,
       },
       {
         title: "Help center",
+        route: "/help-center",
         icon: "ri-question-line",
         shortDesc: "Get help through tutorials and guides",
         commingSoon: true,
       },
       {
         title: "Community",
+        route: "/community",
         icon: "ri-team-line",
         shortDesc: "Share, connect and learn from other customers",
         commingSoon: true,
