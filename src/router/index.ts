6e1641ff6eff04ebd828e2faf9b61395d9a91308
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-20 12:33:11 +0200

    nav items turned into routerlinks

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+/*-- Import Views --*/
+import HomeView from "@/views/HomeView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    }
-  ]
-})
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/:catchall(.*)*",
+      name: "notfound",
+      component: NotFoundView,
+    },
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    return { top: 0 };
+  },
+});
 
-export default router
+export default router;
