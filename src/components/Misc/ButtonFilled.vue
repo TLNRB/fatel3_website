7391fb8d152bc7d0e04ef9ee1f29cc76d88d1eac
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-22 12:49:46 +0200

    btn component created

---
+++ src/components/Misc/ButtonFilled.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+// Prop handling
+const { text, icon, altText } = defineProps(["text", "icon", "altText"]);
+</script>
+
+<template>
+  <div
+    class="btn w-fit flex justify-center items-center gap-[6px] mx-auto py-[.625rem] text-TextLight border-[1px] rounded-[11px] leading-tight cursor-pointer"
+    :class="
+      icon
+        ? 'gap-[6px] pl-[1rem] pr-[.625rem] xxl:gap-[7px] xxl:pl-[1.25rem] xxl:pr-[13px]'
+        : 'px-[1rem] xxl:px-[1.25rem]'
+    "
+  >
+    <div>{{ text }}</div>
+    <img
+      v-if="icon"
+      :src="icon"
+      :alt="altText"
+      class="btn-arrow h-[18px] flex justify-center items-center xxl:h-[20px]"
+    />
+  </div>
+</template>
+
+<style scoped>
+.btn {
+  background-color: var(--ltPrimary);
+  border-color: var(--ltPrimary);
+  transition: border-color, background-color 0.15s ease-in-out;
+}
+
+.btn-arrow {
+  transform: translateY(0.5px) rotate(0);
+  transition: all 0.3s ease-in-out;
+}
+
+/*-- Hover isn't visible on touchscreen devices --*/
+@media (hover: hover) {
+  .btn:hover {
+    background-color: var(--ltPrimaryDark);
+    border-color: var(--ltPrimaryDark);
+  }
+
+  .btn:hover .btn-arrow {
+    transform: translateY(0.5px) rotate(45deg);
+  }
+}
+</style>
