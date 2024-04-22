2218d58857ee0a0b026c3c8afc062506187dce9b
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-22 11:55:34 +0200

    hero text design and layout

--- src/components/HeroSection/Hero.vue
+++ src/components/HeroSection/Hero.vue
@@ -1,21 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import arrowIcon from "@/assets/images/arrow-right-up-line.svg";
+</script>
 
 <template>
-  <section class="px-[1rem] lg:px-[2rem] xxl:w-[1396px] xxl:mx-auto xxl:px-0">
-    <div>
-      <div>
-        <h1>Hero Section</h1>
-        <div></div>
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </div>
-      <div
-        class="btn w-fit flex justify-center items-center py-[.5rem] px-[1rem] text-TextLight border-[1px] rounded-[10px] leading-tight"
+  <section
+    class="pt-[3rem] px-[1rem] flex flex-col items-center gap-[4rem] sm:pt-[4rem] lg:px-[2rem] xl:pt-[6rem] xxl:w-[1396px] xxl:gap-[6rem] xxl:mx-auto xxl:px-0"
+  >
+    <div
+      class="flex flex-col gap-[2.5rem] lg:w-[100%] lg:flex-row lg:justify-between lg:items-center lg:flex-wrap xl:w-auto xl:gap-[6rem] xxl:w-[100%] xxl:gap-[8rem]"
+    >
+      <h1
+        class="text-center text-[3rem] font-[500] leading-[1.25] sm:w-[525px] sm:text-[4rem] sm:leading-[1.2] lg:text-start xxl:w-[750px] xxl:text-[5.5rem]"
       >
-        <div>Get Started</div>
-        <i class="ri-arrow-right-up-line"></i>
+        Manage <span class="font-[500] text-ltPrimary">Your</span> Inventory
+        <span
+          class="px-[.5rem] bg-BGSemiNormal font-[500] rounded-[12px] sm:px-[.625rem] sm:rounded-[16px] xxl:px-[.75rem] xxl:rounded-[22px]"
+          >Easier</span
+        >
+      </h1>
+      <div
+        class="flex flex-col gap-[2.5rem] sm:w-[525px] lg:w-[425px] lg:gap-[2rem] xl:w-[500px]"
+      >
+        <div
+          class="text-center text-[1rem] font-light text-TextNormal sm:text-[1.125rem] sm:leading-snug lg:text-start xxl:text-[1.25rem]"
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </div>
+        <div
+          class="btn w-fit flex justify-center items-center gap-[6px] mx-auto py-[.625rem] pl-[1rem] pr-[.625rem] text-TextLight border-[1px] rounded-[11px] leading-tight cursor-pointer lg:ml-0 xxl:gap-[7px] xxl:py-[.75rem] xxl:pl-[1.25rem] xxl:pr-[13px] xxl:text-[1.125rem] xxl:rounded-[13px]"
+        >
+          <div>Get Started</div>
+          <img
+            :src="arrowIcon"
+            alt="right up arrow icon"
+            class="btn-arrow h-[18px] flex justify-center items-center xxl:h-[20px]"
+          />
+        </div>
       </div>
     </div>
     <div>
@@ -28,7 +49,12 @@
 .btn {
   background-color: var(--ltPrimary);
   border-color: var(--ltPrimary);
-  transition: all 0.15s ease-in-out;
+  transition: border-color, background-color 0.15s ease-in-out;
+}
+
+.btn-arrow {
+  transform: translateY(0.5px) rotate(0);
+  transition: all 0.3s ease-in-out;
 }
 
 /*-- Hover isn't visible on touchscreen devices --*/
@@ -36,7 +62,10 @@
   .btn:hover {
     background-color: var(--ltPrimaryDark);
     border-color: var(--ltPrimaryDark);
-    transition: all 0.15s ease-in-out;
+  }
+
+  .btn:hover .btn-arrow {
+    transform: translateY(0.5px) rotate(45deg);
   }
 }
 </style>
