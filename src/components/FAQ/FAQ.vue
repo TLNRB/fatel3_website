3ef4779450ba7e530b6d2c6ab8819dc7f678db38
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-22 16:25:37 +0200

    faq section created

---
+++ src/components/FAQ/FAQ.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+/*-- Import components --*/
+import ButtonFilled from "@/components/Misc/ButtonFilled.vue";
+/*-- Import assets --*/
+import arrowIcon from "@/assets/icons/arrow-right-up-line.svg";
+</script>
+
+<template>
+  <section
+    class="pt-[3rem] flex flex-col items-center gap-[4rem] sm:pt-[4rem] xl:gap-[6rem] xl:pt-[6rem] xxl:w-[1396px] xxl:mx-auto"
+  ></section>
+</template>
+
+<style scoped></style>
