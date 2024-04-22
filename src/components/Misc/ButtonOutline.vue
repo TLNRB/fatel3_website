121dc39a4ee1d1047e4c90a55ffccd898d36b0d6
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-22 16:37:49 +0200

    btn ouytline component

---
+++ src/components/Misc/ButtonOutline.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+// Prop handling
+const { text, theme } = defineProps(["text", "theme"]);
+</script>
+
+<template>
+  <div
+    class="w-fit flex justify-center items-center gap-[6px] mx-auto py-[.625rem] px-[1rem] bg-transparent border-[1px] rounded-[11px] leading-tight cursor-pointer xxl:px-[1.25rem]"
+    :class="theme === 'light' ? 'btn-outline-light' : 'btn-outline-dark'"
+  >
+    <div>{{ text }}</div>
+  </div>
+</template>
+
+<style scoped>
+.btn-outline-light {
+  color: var(--TextSemiDark);
+  border-color: var(--ltBorderNormal);
+  transition: border-color 0.15s ease-in-out;
+}
+
+.btn-outline-dark {
+  color: var(--TextLight);
+  border-color: var(--TextLight);
+  transition: border-color 0.15s ease-in-out;
+}
+
+/*-- Hover isn't visible on touchscreen devices --*/
+@media (hover: hover) {
+  .btn-outline-light:hover {
+    border-color: var(--ltPrimary);
+  }
+
+  .btn-outline-dark:hover {
+    border-color: var(--ltPrimaryDark);
+  }
+}
+</style>
