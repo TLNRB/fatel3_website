0b63cd89cc6297929355daf0d2b4ab7cf3255dc3
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-22 12:49:46 +0200

    btn component created

--- src/components/HeroSection/Hero.vue
+++ src/components/HeroSection/Hero.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
+/*-- Import components --*/
+import ButtonFilled from "@/components/Misc/ButtonFilled.vue";
+/*-- Import assets --*/
 import arrowIcon from "@/assets/images/arrow-right-up-line.svg";
 import tempImg from "@/assets/images/temp-fatel3.png";
 </script>
 
 <template>
   <section
-    class="pt-[3rem] flex flex-col items-center gap-[4rem] sm:pt-[4rem] xl:pt-[6rem] xxl:w-[1396px] xxl:gap-[6rem] xxl:mx-auto"
+    class="pt-[3rem] flex flex-col items-center gap-[4rem] sm:pt-[4rem] xl:gap-[6rem] xl:pt-[6rem] xxl:w-[1396px] xxl:mx-auto"
   >
     <div
       class="flex flex-col gap-[2.5rem] px-[1rem] lg:w-[100%] lg:flex-row lg:justify-between lg:items-center lg:flex-wrap lg:px-[2rem] xl:w-auto xl:gap-[6rem] xxl:w-[100%] xxl:gap-[8rem] xxl:px-0"
@@ -28,16 +31,12 @@ import tempImg from "@/assets/images/temp-fatel3.png";
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </div>
-        <div
-          class="btn w-fit flex justify-center items-center gap-[6px] mx-auto py-[.625rem] pl-[1rem] pr-[.625rem] text-TextLight border-[1px] rounded-[11px] leading-tight cursor-pointer lg:ml-0 xxl:gap-[7px] xxl:py-[.75rem] xxl:pl-[1.25rem] xxl:pr-[13px] xxl:text-[1.125rem] xxl:rounded-[13px]"
-        >
-          <div>Get Started</div>
-          <img
-            :src="arrowIcon"
-            alt="right up arrow icon"
-            class="btn-arrow h-[18px] flex justify-center items-center xxl:h-[20px]"
-          />
-        </div>
+        <ButtonFilled
+          text="Get Started"
+          :icon="arrowIcon"
+          altText="right up arrow icon"
+          class="lg:ml-0 xxl:py-[.75rem] xxl:text-[1.125rem] xxl:rounded-[13px]"
+        />
       </div>
     </div>
     <div
@@ -52,27 +51,4 @@ import tempImg from "@/assets/images/temp-fatel3.png";
   </section>
 </template>
 
-<style scoped>
-.btn {
-  background-color: var(--ltPrimary);
-  border-color: var(--ltPrimary);
-  transition: border-color, background-color 0.15s ease-in-out;
-}
-
-.btn-arrow {
-  transform: translateY(0.5px) rotate(0);
-  transition: all 0.3s ease-in-out;
-}
-
-/*-- Hover isn't visible on touchscreen devices --*/
-@media (hover: hover) {
-  .btn:hover {
-    background-color: var(--ltPrimaryDark);
-    border-color: var(--ltPrimaryDark);
-  }
-
-  .btn:hover .btn-arrow {
-    transform: translateY(0.5px) rotate(45deg);
-  }
-}
-</style>
+<style scoped></style>
