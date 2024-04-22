83034bf2a9004ac2f0663d11acaa0ccbd6d4a480
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-22 16:31:08 +0200

    faq layout

--- src/components/FAQ/FAQ.vue
+++ src/components/FAQ/FAQ.vue
@@ -1,14 +1,38 @@
 <script setup lang="ts">
 /*-- Import components --*/
-import ButtonFilled from "@/components/Misc/ButtonFilled.vue";
-/*-- Import assets --*/
-import arrowIcon from "@/assets/icons/arrow-right-up-line.svg";
+
+// FAQs
+const faq = [
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod, consectetur adipiscing elit, sed do eiusmod?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+];
 </script>
 
 <template>
   <section
-    class="pt-[3rem] flex flex-col items-center gap-[4rem] sm:pt-[4rem] xl:gap-[6rem] xl:pt-[6rem] xxl:w-[1396px] xxl:mx-auto"
-  ></section>
+    class="pt-[5rem] px-[1rem] flex flex-col items-center gap-[1.5rem] lg:px-[2rem] xl:gap-[2rem] xl:pt-[8rem] xxl:w-[1396px] xxl:mx-auto xxl:px-0"
+  >
+    <h2>Frequently Asked Questions</h2>
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua.
+    </div>
+  </section>
 </template>
 
 <style scoped></style>
