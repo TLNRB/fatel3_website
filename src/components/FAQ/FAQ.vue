c4a9f6ca1d2cac74b7e89cd81c59619ccd9c4dad
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-23 10:31:15 +0200

    faq large screen optimized

--- src/components/FAQ/FAQ.vue
+++ src/components/FAQ/FAQ.vue
@@ -12,20 +12,17 @@ const faqs: any = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod?",
     answer:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    height: 115,
   },
   {
     question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
     answer:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-    height: 135,
   },
   {
     question:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod, consectetur adipiscing elit, sed do eiusmod?",
     answer:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    height: 77,
   },
 ];
 
@@ -44,45 +41,50 @@ const toggleActiveFAQ = (index: number) => {
   <section
     class="pt-[5rem] pb-[4rem] px-[1rem] flex flex-col items-center gap-[1.5rem] lg:px-[2rem] xl:gap-[2rem] xl:pt-[8rem] xl:pb-[5rem] xxl:w-[1396px] xxl:mx-auto xxl:px-0"
   >
-    <h2 class="text-center text-[2rem] font-[500] leading-tight">
+    <h2
+      class="w-[100%] text-center text-[2rem] font-[500] leading-[1.2] xs:w-[300px] lg:w-[550px] lg:text-[3rem] xxl:text-[4rem]"
+    >
       Frequently Asked Questions
     </h2>
-    <div class="text-center text-TextNormal font-light leading-snug">
+    <div
+      class="w-[100%] text-center text-TextNormal font-light leading-snug xs:w-[325px] sm:w-[400px] lg:w-[500px] lg:text-[1.125rem] xxl:w-[550px] xxl:text-[1.25rem]"
+    >
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua.
     </div>
-    <div class="flex flex-col gap-[1.125rem] my-[1.5rem]">
+    <div
+      class="flex flex-col gap-[1rem] my-[1.5rem] xl:gap-[1.125rem] xl:my-[2rem]"
+    >
       <div
         v-for="(faq, index) in faqs"
         :key="index"
-        class="p-[1.25rem] border-[1px] border-ltPrimary rounded-[14px]"
+        class="w-[100%] p-[1.25rem] border-[1px] border-ltPrimary rounded-[14px] md:w-[725px] lg:w-[775px] lg:p-[1.5rem] xxl:w-[825px] xxl:px-[1.75rem]"
       >
-        <div class="flex items-center gap-[1.25rem]">
-          <div class="leading-tight">
+        <div
+          class="flex justify-between items-center gap-[1.25rem] lg:gap-[1.5rem] xxl:gap-[1.75rem]"
+        >
+          <div class="leading-tight lg:text-[1.125rem] xxl:text-[1.25rem]">
             {{ faq.question }}
           </div>
           <div
-            class="btn min-w-[30px] min-h-[30px] w-[30px] h-[30px] p-[5px] bg-ltPrimary rounded-[10px] cursor-pointer translate-x-[1px]"
+            @click="toggleActiveFAQ(index)"
+            class="btn min-w-[30px] min-h-[30px] w-[30px] h-[30px] p-[5px] bg-ltPrimary rounded-[10px] cursor-pointer duration-[.15s] ease-in-out translate-x-[1px] xxl:min-w-[34px] xxl:min-h-[34px] xxl:w-[34px] xxl:h-[34px] xxl:p-[6px] xxl:rounded-[11px]"
           >
             <img
               :src="addIcon"
               alt="icon"
               class="btn-icon"
               :class="activeFAQIndex === index ? 'btn-icon-active' : ''"
-              @click="toggleActiveFAQ(index)"
             />
           </div>
         </div>
         <div
-          class="text-[.875rem] font-light text-TextNormal leading-snug overflow-hidden duration-[.3s] ease-in-out"
+          class="text-[.875rem] font-light text-TextNormal leading-snug overflow-hidden lg:text-[15px] xxl:text-[1rem]"
           :class="
             activeFAQIndex === index
-              ? 'mt-[1.25rem] visible opacity-[1]'
-              : 'invisible opacity-0'
+              ? 'h-auto mt-[1.25rem] visible opacity-[1]'
+              : 'h-0 invisible opacity-0'
           "
-          :style="{
-            height: activeFAQIndex === index ? faq.height + 'px' : '0',
-          }"
         >
           {{ faq.answer }}
         </div>
@@ -104,8 +106,8 @@ const toggleActiveFAQ = (index: number) => {
 
 /*-- Hover isn't visible on touchscreen devices --*/
 @media (hover: hover) {
-  .btn:hover .btn-icon {
-    transform: rotate(45deg);
+  .btn:hover {
+    background-color: var(--ltPrimaryDark);
   }
 }
 </style>
