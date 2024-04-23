a00dabd1d77cf4e2ff73df2f26c3f5e7d9e8a128
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-23 10:47:51 +0200

    section type comp created

---
+++ src/components/Misc/SectionType.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// Prop handling
+const { text } = defineProps(["text"]);
+</script>
+
+<template>
+  <div
+    class="py-[.25rem] px-[.625rem] bg-ltPrimary text-[.75rem] text-TextLight rounded-[6px] leading-tight xxl:px-[.75rem] xxl:text-[.875rem] xxl:rounded-[7px]"
+  >
+    {{ text }}
+  </div>
+</template>
+
+<style scoped></style>
