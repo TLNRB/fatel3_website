45cd182338f987e2e85d7537309377d6e4b7fdfb
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-23 11:03:49 +0200

    reviews title design

--- src/components/Misc/SectionType.vue
+++ src/components/Misc/SectionType.vue
@@ -5,7 +5,7 @@ const { text } = defineProps(["text"]);
 
 <template>
   <div
-    class="py-[.25rem] px-[.625rem] bg-ltPrimary text-[.75rem] text-TextLight rounded-[6px] leading-tight xxl:px-[.75rem] xxl:text-[.875rem] xxl:rounded-[7px]"
+    class="w-fit py-[.25rem] px-[.625rem] bg-ltPrimary text-[.75rem] text-TextLight rounded-[6px] leading-tight xxl:px-[.75rem] xxl:text-[.875rem] xxl:rounded-[7px]"
   >
     {{ text }}
   </div>
