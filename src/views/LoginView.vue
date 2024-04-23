3a408db49a6dcd3b9d92304db15a8219a27f07f3
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-23 15:27:52 +0200

    login design further

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -16,7 +16,9 @@ import logoPrimary from "@/assets/images/logo_primary.svg";
       class="w-[250px] py-[2rem] px-[1.25rem] border-[1px] border-ltPrimary rounded-[10px] xs:w-[325px] xl:w-[400px] xl:py-[2.5rem] xl:px-[2rem]"
     >
       <h2 class="text-[1.125rem] font-[500]">Log in to your account</h2>
-      <div class="mt-[1.25rem] mb-[2.5rem] xl:mt-[1.5rem] xl:mb-[3rem]">
+      <div
+        class="flex flex-col gap-[1rem] mt-[1.25rem] mb-[.625rem] xl:mt-[1.5rem] xl:mb-[.75rem]"
+      >
         <input
           type="email"
           name=""
@@ -24,9 +26,26 @@ import logoPrimary from "@/assets/images/logo_primary.svg";
           placeholder="example@email.com"
           class="input-field w-[100%] py-[.5rem] px-[.875rem] text-[.875rem] font-light border-[1px] leading-tight rounded-[5px] outline-none xl:py-[.625rem] xl:px-[1rem] xl:text-[15px] xl:rounded-[6px]"
         />
+        <div class="relative">
+          <input
+            type="password"
+            name=""
+            id=""
+            placeholder="Password"
+            class="input-field w-[100%] py-[.5rem] px-[.875rem] text-[.875rem] font-light border-[1px] leading-tight rounded-[5px] outline-none xl:py-[.625rem] xl:px-[1rem] xl:text-[15px] xl:rounded-[6px]"
+          />
+          <i
+            class="ri-eye-off-line text-[1.125rem] h-[18px] flex justify-center items-center absolute top-[50%] right-[.875rem] translate-y-[-50%] text-TextSemiNormal cursor-pointer xl:text-[1.25rem] xl:h-[20px] xl:right-[1rem]"
+          ></i>
+        </div>
       </div>
       <div
-        class="btn w-[100%] flex justify-center items-center py-[.5rem] bg-ltPrimary text-TextLight cursor-pointer rounded-[8px] duration-[.15s] ease-in-out xl:py-[9px] xl:text-[17px] xl:rounded-[9px]"
+        class="text-right text-[.875rem] font-light text-ltPrimary cursor-pointer"
+      >
+        Forgot password?
+      </div>
+      <div
+        class="btn w-[100%] mt-[1.25rem] flex justify-center items-center py-[.5rem] bg-ltPrimary text-TextLight cursor-pointer rounded-[8px] duration-[.15s] ease-in-out xl:mt-[1.5rem] xl:py-[9px] xl:text-[17px] xl:rounded-[9px]"
       >
         Log in
       </div>
