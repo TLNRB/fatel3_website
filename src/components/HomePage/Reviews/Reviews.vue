32c8292cc09d36058d84f36bc7fb5d526d1bea66
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-23 11:03:49 +0200

    reviews title design

--- src/components/HomePage/Reviews/Reviews.vue
+++ src/components/HomePage/Reviews/Reviews.vue
@@ -5,14 +5,20 @@ import SectionType from "@/components/Misc/SectionType.vue";
 
 <template>
   <section
-    class="pt-[5rem] pb-[4rem] px-[1rem] flex flex-col items-center gap-[1.5rem] lg:px-[2rem] xl:gap-[2rem] xl:pt-[8rem] xl:pb-[5rem] xxl:w-[1396px] xxl:mx-auto xxl:px-0"
+    class="py-[4rem] px-[1rem] bg-BGSemiDark xs:mx-[1rem] xs:rounded-[24px] lg:mx-[2rem] lg:px-[2rem] xl:py-[5rem] xxl:w-[1396px] xxl:mx-auto"
   >
-    <SectionType text="reviews" />
-    <h2
-      class="w-[100%] text-center text-[2rem] font-[500] leading-[1.2] xs:w-[300px] lg:w-[550px] lg:text-[3rem] xxl:text-[4rem]"
-    >
-      Frequently Asked Questions
-    </h2>
+    <div class="flex flex-col items-center gap-[3rem] xl:gap-[4rem]">
+      <div class="flex flex-col items-center gap-[1rem] xl:gap-[1.25rem]">
+        <SectionType text="reviews" />
+        <h2
+          class="w-[100%] text-center text-[2rem] text-TextLight font-[500] leading-[1.2] xs:w-[300px] lg:w-[500px] lg:text-[3rem] xxl:w-[550px] xxl:text-[4rem]"
+        >
+          What Others Say about
+          <span class="font-[500] text-ltPrimary">fatel3</span>
+        </h2>
+      </div>
+      <div></div>
+    </div>
   </section>
 </template>
 
