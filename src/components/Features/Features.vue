b18b26593eea7ce892970e4bcd03c261d595b605
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-24 17:39:22 +0200

    features section

--- src/components/Features/Features.vue
+++ src/components/Features/Features.vue
@@ -2,10 +2,10 @@
 import { RouterLink } from "vue-router";
 /*-- Import Components --*/
 import SectionType from "@/components/Misc/SectionType.vue";
-import ButtonFilled from "@/components/Buttons/ButtonFilled.vue";
-import ButtonOutlined from "@/components/Buttons/ButtonOutlined.vue";
+import ButtonFilled from "@/components/Misc/ButtonFilled.vue";
+import ButtonOutline from "@/components/Misc/ButtonOutline.vue";
 /*-- Import assets --*/
-import arrowIcon from "@/assets/icons/arrow-right-up-line-primary.svg";
+import arrowIcon from "@/assets/icons/arrow-right-up-line.svg";
 /*-- Import data --*/
 import featuresData from "@/data/featuresData";
 </script>
@@ -28,8 +28,58 @@ import featuresData from "@/data/featuresData";
           <span class="font-[500] text-ltPrimary">Your</span> Inventorying
         </h2>
       </div>
+      <div
+        class="flex justify-center items-center gap-[1rem] flex-wrap sm:gap-[2rem] xl:gap-[2.5rem]"
+      >
+        <RouterLink
+          v-for="(feature, index) in featuresData"
+          :key="index"
+          to="/features"
+          class="feature w-[100px] flex flex-col items-center gap-[.75rem] cursor-pointer"
+        >
+          <div
+            class="feature-icon-container min-w-[56px] min-h-[56px] w-[56px] h-[56px] flex justify-center items-center bg-transparent rounded-[18px] duration-[.3s] ease-in-out"
+          >
+            <i
+              class="feature-icon text-[2rem] h-[32px] flex justify-center items-center text-TextNormal translate-y-[.5px] duration-[.3s] ease-in-out"
+              :class="feature.icon"
+            ></i>
+          </div>
+          <div
+            class="feature-icon-text text-center text-[.875rem] font-light text-TextNormal leading-tight duration-[.3s] ease-in-out"
+          >
+            {{ feature.title }}
+          </div>
+        </RouterLink>
+      </div>
+      <div
+        class="flex justify-center items-center gap-[.75rem] flex-wrap sm:gap-[1rem]"
+      >
+        <ButtonOutline text="See all features" theme="dark" route="/features" />
+        <ButtonFilled
+          text="Get Started"
+          :icon="arrowIcon"
+          altText="arrow icon"
+          route="/support"
+        />
+      </div>
     </div>
   </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+/*-- Hover isn't visible on touchscreen devices --*/
+@media (hover: hover) {
+  .feature:hover .feature-icon-container {
+    background-color: var(--ltPrimary);
+  }
+
+  .feature:hover .feature-icon {
+    color: var(--TextLight);
+  }
+
+  .feature:hover .feature-icon-text {
+    color: var(--TextLight);
+  }
+}
+</style>
