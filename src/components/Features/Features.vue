1486373c0a4b0043b84434de1c9668b52d1dedd2
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-24 17:04:42 +0200

    margin update

--- src/components/Features/Features.vue
+++ src/components/Features/Features.vue
@@ -11,10 +11,24 @@ import featuresData from "@/data/featuresData";
 </script>
 
 <template>
-  <section class="relative py-[4rem] bg-BGDark xl:py-[5rem] xxl:pb-[8rem]">
+  <section class="relative py-[4rem] bg-BGDark xl:py-[6rem] xxl:py-[7rem]">
     <div
-      class="flex flex-col items-center gap-[2rem] px-[1rem] lg:px-[2rem] lg:w-[100%] lg:items-start xl:gap-[3rem] xxl:w-[1396px] xxl:mx-auto"
-    ></div>
+      class="flex flex-col items-center gap-[3rem] px-[1rem] lg:px-[2rem] xl:gap-[4rem] xxl:w-[1396px] xxl:mx-auto"
+    >
+      <div class="flex flex-col items-center gap-[1rem] xl:gap-[1.25rem]">
+        <SectionType
+          text="features"
+          bgColor="bg-ltPrimary"
+          textColor="text-TextLight"
+        />
+        <h2
+          class="w-[100%] text-center text-[2rem] text-TextLight font-[500] leading-[1.15] xs:w-[300px] lg:w-[500px] lg:text-[3rem] xxl:w-[600px] xxl:text-[4rem]"
+        >
+          Powerful Tools for
+          <span class="font-[500] text-ltPrimary">Your</span> Inventorying
+        </h2>
+      </div>
+    </div>
   </section>
 </template>
 
