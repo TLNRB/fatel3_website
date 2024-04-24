26264aa33c95d1c963f13695fc718693deaa93d3
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-24 16:50:15 +0200

    features section created

---
+++ src/components/Features/Features.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+/*-- Import Components --*/
+import SectionType from "@/components/Misc/SectionType.vue";
+import ButtonFilled from "@/components/Buttons/ButtonFilled.vue";
+import ButtonOutlined from "@/components/Buttons/ButtonOutlined.vue";
+/*-- Import assets --*/
+import arrowIcon from "@/assets/icons/arrow-right-up-line-primary.svg";
+/*-- Import data --*/
+import featuresData from "@/data/featuresData";
+</script>
+
+<template>
+  <section class="relative py-[4rem] bg-BGDark xl:py-[5rem] xxl:pb-[8rem]">
+    <div
+      class="flex flex-col items-center gap-[2rem] px-[1rem] lg:px-[2rem] lg:w-[100%] lg:items-start xl:gap-[3rem] xxl:w-[1396px] xxl:mx-auto"
+    ></div>
+  </section>
+</template>
+
+<style scoped></style>
