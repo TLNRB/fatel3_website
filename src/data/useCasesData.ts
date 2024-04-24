4d12cf3f2d1e6a875941cfb34cd1d13d464b854a
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-24 09:53:12 +0200

    cards on use cases designed

--- src/data/useCasesData.ts
+++ src/data/useCasesData.ts
@@ -4,6 +4,8 @@ const useCases = [
     route: "/reselling",
     icon: "ri-price-tag-3-line",
     shortDesc: "For the ones reselling various items and products",
+    longDesc:
+      "Ambitious resellers who sell different types of products and seek for a system that helps grow their business.",
     commingSoon: false,
   },
   {
@@ -11,6 +13,8 @@ const useCases = [
     route: "/e-commerce",
     icon: "ri-shopping-cart-line",
     shortDesc: "Integration with the most used e-commerce platforms",
+    longDesc:
+      "Having a website on one of the popular e-commerce platforms or having your own, doesn't matter, we are here to help.",
     commingSoon: true,
   },
 ];
