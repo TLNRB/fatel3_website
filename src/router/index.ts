937381737d1ec102865dcb00cb5306f2fbd17cb0
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-04-25 10:43:24 +0200

    features page dynamic sub menus

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 /*-- Import Views --*/
 import HomeView from "@/views/HomeView.vue";
+import AllFeaturesView from "@/views/AllFeaturesView.vue";
 import LoginView from "@/views/LoginView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 
@@ -11,6 +12,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/features",
+      name: "features",
+      component: AllFeaturesView,
     },
     {
       path: "/login",
